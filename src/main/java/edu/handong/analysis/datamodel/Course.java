a08c1058963ac907ed9cc26beea42de4b0014157
JC <jc@lifove.net>
2019-05-10 08:31:04 +0900

    Add Student and Course classes

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,13 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	private String courseName;
+	
+	public Course(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	public String getCourseName() {
+		return courseName;
+	}
+}
